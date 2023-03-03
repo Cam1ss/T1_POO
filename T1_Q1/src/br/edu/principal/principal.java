b5482c1870486b5b3cb46adbf3c4be1a7c2f1145
Camila Braúna <125037138+Cam1ss@users.noreply.github.com>
2023-03-03 10:44:09 -0300

    Update principal.java

--- T1_Q1/src/br/edu/principal/principal.java
+++ T1_Q1/src/br/edu/principal/principal.java
@@ -7,16 +7,16 @@ public class principal {
 		 * 1. Faça um programa que receba quatro numeros inteiros, calcule e mostre a soma desses numeros. 
 		 */
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite sua n1: ");
+		System.out.println("Digite um n1: ");
 		int n1 = sc.nextInt();
-		System.out.println("Digite sua n2: ");
+		System.out.println("Digite um n2: ");
 	    int n2 = sc.nextInt();
-	    System.out.println("Digite sua n3: ");
+	    System.out.println("Digite um n3: ");
 	    int n3 = sc.nextInt();
-	    System.out.println("Digite sua n4: ");
+	    System.out.println("Digite um n4: ");
 	    int n4 = sc.nextInt();
 	    int soma = n1 + n2 + n3 + n4;
-	    System.out.println("A soma desses 4 números são: "+ soma);
+	    System.out.println("Essa é a Soma desses 4 números: "+ soma);
 		
 	}
 
